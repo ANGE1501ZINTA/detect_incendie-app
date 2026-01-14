4d5ee9bc4a49dc07c64d4ee7dc9b7b017bdfe8d8
arthur zinta <jobszinta@gmail.com>
2026-01-14 02:08:10 -1200

    loa

--- api.py
+++ api.py
@@ -25,8 +25,12 @@ model.load_state_dict(torch.load("fire_cnn_bottleneck.pth", map_location="cpu"))
 model.eval()
 
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((224, 224)),   
     transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
 ])
 
 labels = ["Fire", "No Fire"]
